41b6002f6ec332448f534206b988d27af254a953
EnrickyHip <93732489+EnrickyHip@users.noreply.github.com>
2021-12-03 17:10:30 -0300

    aa

---
+++ actions/login_act.php
@@ -0,0 +1,18 @@
+<?php
+
+    include_once "../vendor/autoload.php";
+
+    if (isset($_POST['login'])){
+
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_EMAIL);
+        $pwd = filter_input(INPUT_POST, "pwd");
+
+        $signup_ctrl = new \classes\controler\Signup_ctrl($username, $email, $pwd); 
+        $signup_ctrl->create_user();
+    }
+
+    else{
+        header("Location: ../register.php");
+        exit();
+    }
