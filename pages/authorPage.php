89f871588cb0fe265c05ebb65f7c83466f5662e2
Captain Keys <89275202+CaptainKeys21@users.noreply.github.com>
2021-12-05 19:57:35 -0300

    Update authorPage.php

--- pages/authorPage.php
+++ pages/authorPage.php
@@ -1,3 +1,16 @@
 <div class="text-center">
     <img src="./images/Avatar_PlaceHolder.png" alt="Foto de Perfil" id="avIcon" class="rounded-circle border border-5">
 </div>
+<div class="text-center">
+    <?php
+        #aqui deveria trazer o nome de usuário e nome artístico mas eu não sei fazer KKKKKKKK
+    ?>
+    <span><h3>Nome artístico</h3></span>
+    <span><h5>@nome_de_usuário</h5></span>  
+</div>
+<div>
+    <h2>Sobre o Autor:</h2>
+    <article>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, velit deserunt? Similique officia ab numquam. Accusantium facere voluptatem consectetur obcaecati, unde ullam exercitationem fuga recusandae amet dolore aliquid. Hic, cum!
+    </article>
+</div>
