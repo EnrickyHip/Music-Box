fab14875bf915dd2a1243abb087525de30a71f4d
Captain Keys <89275202+CaptainKeys21@users.noreply.github.com>
2022-01-28 19:57:16 -0300

    Footer

---
+++ includes/footer_inc.php
@@ -0,0 +1,11 @@
+<div class="container">
+  <footer class="py-3 my-3">
+    <ul class="nav justify-content-center border-bottom mb-2">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Início</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Sobre</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Políticas de Privacidade</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">termos e Condições</a></li>
+    </ul>
+    <p class="text-center text-muted">© 2022 Music Box</p>
+  </footer>
+</div>
--- templates/main.php
+++ templates/main.php
@@ -83,6 +83,10 @@
         
     </main>
 
+    <?php
+        require_once "includes/footer_inc.php";
+    ?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="../script/validate/validate_login.js"></script>
     <script src="../script/ajaxify_call.js"></script>
