ed43f6dc131366a710e2dd83284345b5239dbc3b
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-05-12 09:06:51 -0300

    identando o SongControler

--- classes/controler/SongControler.php
+++ classes/controler/SongControler.php
@@ -2,8 +2,8 @@
 
     namespace classes\controler;
 
-use classes\model\AlbumModel;
-use \classes\model\SongModel;
+    use classes\model\AlbumModel;
+    use \classes\model\SongModel;
 
     require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
 
@@ -24,33 +24,32 @@ use \classes\model\SongModel;
 
             if(in_array($ext, $this->formatos)){
 
-                if ($song['error'] === 0){
+                if ($song['error']) return;
                     
-                    $song_file_name = $_SESSION[$song_code_name]['file_name']; //$_SESSION['actual_song']['file_name'] refere-se ao nome do arquivo, $_SESSION['actual_song'] é um array com as informações da música, foi criado em song_register.
-                    $song_file = "songs/".$song_file_name; // define a pasta de upload
-                    $temporario = "../temp_songs/".$song_file_name; //pasta temporária
-                    rename($temporario, "../$song_file");// move o arquivo do local temoporario para a pastas
-                    $_SESSION[$song_code_name] = null;
+                $song_file_name = $_SESSION[$song_code_name]['file_name']; //$_SESSION['actual_song']['file_name'] refere-se ao nome do arquivo, $_SESSION['actual_song'] é um array com as informações da música, foi criado em song_register.
+                $song_file = "songs/".$song_file_name; // define a pasta de upload
+                $temporario = "../temp_songs/".$song_file_name; //pasta temporária
+                rename($temporario, "../$song_file");// move o arquivo do local temoporario para a pastas
+                $_SESSION[$song_code_name] = null;
 
-                    if(!$album_id){ //caso o usuário não tenha selecionado álbum, o sistema irá criar um solo para a música
-                        $album_id = $this->createSoloAlbum($song_title);
-                        $single = true;
-                    }
-                    else {
-                        $single = false;
-                    }
-
-                    $this->insert_song($song_code_name, $song_title,  $song_file, $visibility, $single, $this->user_id, $album_id, $song_desc, $genre, $subgenre);
-
-                    if(!$single){
-                        $playlist_code_name = AlbumModel::get_album_info($album_id, "playlist_code_name")['playlist_code_name'];
-
-                        $playlist_controler = new \classes\controler\PlaylistControler($this->user_id);
-                        $playlist_controler->add_song($playlist_code_name, [$song_code_name]);
-                    }
-
-                    return $song_code_name;
+                if(!$album_id){ //caso o usuário não tenha selecionado álbum, o sistema irá criar um solo para a música
+                    $album_id = $this->createSoloAlbum($song_title);
+                    $single = true;
                 }
+                else {
+                    $single = false;
+                }
+
+                $this->insert_song($song_code_name, $song_title,  $song_file, $visibility, $single, $this->user_id, $album_id, $song_desc, $genre, $subgenre);
+
+                if(!$single){
+                    $playlist_code_name = AlbumModel::get_album_info($album_id, "playlist_code_name")['playlist_code_name'];
+
+                    $playlist_controler = new \classes\controler\PlaylistControler($this->user_id);
+                    $playlist_controler->add_song($playlist_code_name, [$song_code_name]);
+                }
+
+                return $song_code_name;  
             }
         }
 
